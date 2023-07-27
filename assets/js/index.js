0a30ec02940ffbd671f45063db8e9a66e29f9ee8
S4nt1nO <Gianluca1996@gmx.de>
2023-07-27 11:33:15 +0200

    starting 5 days forecast

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,33 @@
 'use strict'
 
-// 956a2497a65c68420a0a17e6efc0dddc
+import { apiKey } from './api.js'
 
 const weatherOutput = document.querySelector('.weather-output')
 const inputSection = document.querySelector('.input-section')
-const inputCountry = document.querySelector('#country')
 const inputCity = document.querySelector('#city')
 const checkBtn = document.querySelector('.check')
 
 let lon
 let lat
 
+const getLocalTime = (localTimeInS, timeZone) => {
+  const offSetInMs = new Date().getTimezoneOffset() * 60 * 1000
+  const date1 = new Date(localTimeInS * 1000)
+  const localDate = new Date(date1.getTime() + offSetInMs + timeZone * 1000)
+  return localDate.toLocaleTimeString('de', {
+    hours: '2 digit',
+    minutes: '2 digit',
+    seconds: '2 digit',
+  })
+}
+
 const checkWeather = () => {
-  const countryValue = inputCountry.value.toLowerCase()
   const cityValue = inputCity.value.toLowerCase()
 
-  if (cityValue === undefined && countryValue === undefined) {
+  if (cityValue === undefined) {
     return
   } else if (cityValue !== undefined) {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${cityValue},${countryValue}&limit=1&appid=956a2497a65c68420a0a17e6efc0dddc`,
-    )
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&limit=1&appid=${apiKey}`)
       .then(response => {
         if (!response.ok) {
           throw new Error('something went wrong')
@@ -33,7 +40,7 @@ const checkWeather = () => {
       })
       .then(() =>
         fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=956a2497a65c68420a0a17e6efc0dddc&units=metric&lang=de`,
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=de`,
         )
           .then(response => {
             if (!response.ok) {
@@ -43,23 +50,37 @@ const checkWeather = () => {
           })
           .then(data => {
             console.log(data)
+            const currentTime = getLocalTime(Date.now() / 1000, data.timezone)
+            const sunsetTime = getLocalTime(data.sys.sunset, data.timezone)
+            const sunriseTime = getLocalTime(data.sys.sunrise, data.timezone)
             const weatherHtml = `
-        <h2>Weather in</h2>
-        <h2> ${data.name}</h2>
-        <p>Weather:  <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"> ${data.main.temp}</p>
-        <p>Local time</p>
-        <p>Cloudiness: ${data.weather[0].main}</p>
-        <p>Pressure: ${data.main.pressure}</p>
-        <p>Sunrise ${data.sys.sunrise}</p>
-        <p>Sunset ${data.sys.sunset}</p>
-        <p>Windspeed: ${data.wind.speed}Km/h</p>
-        <p>Geo coords: lan: ${lat.toFixed(2)}, - lon: ${lon.toFixed(2)}</p>
-      `
+            <div>
+              <h2>Weather in</h2>
+              <h2> ${data.name}</h2>
+              <p>Weather:  <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"> ${
+              data.main.temp
+            } °C</p>
+              <p>Feels like: ${data.main.feels_like}</p>
+              <p>Local time: ${currentTime}</p>
+              <p>Humidity: ${data.main.humidity}%</p>
+              <p>Cloudiness: ${data.weather[0].main}</p>
+              <p>Pressure: ${data.main.pressure} hpa</p>
+              <p>Sunrise ${sunriseTime}</p>
+              <p>Sunset ${sunsetTime}</p>
+              <p>Windspeed: ${data.wind.speed}Km/h</p>
+              <p>Geo coordinates: ${lat.toFixed(2)}, ${lon.toFixed(2)}</p>
+              </div>
+              `
             weatherOutput.innerHTML = weatherHtml
           })
           .catch(error => console.log(error)),
       )
       .catch(error => console.log(error))
+      .then(() => {
+        fetch(
+          `https//:api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=de`,
+        )
+      })
   }
 }
 
